51ef2c070617e34499f0cbe77e34d3232fc9ca40
aangelo742 <aangelo742@gmail.com>
2024-02-06 23:50:02 -0600

    Added some code to munge.py to remove blank spaces, and repeating header lines. Does NOT create a new file (yet)

--- munge.py
+++ munge.py
@@ -1,1 +1,13 @@
 # Place code below to do the munging part of this assignment.
+import re
+file = open('data\GLB.Ts+dSST.txt', 'r')
+
+lines_seen_so_far = set()
+
+for line in file:
+    line = line.strip()
+    if len(line) != 0 and line not in lines_seen_so_far:
+        #print(line)
+        new_line = re.sub("\s+", ",", line)
+        print(new_line)
+        lines_seen_so_far.add(line)
